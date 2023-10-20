cdb1bf60428fbac1f38f68a088058f0f27643113
leelasaiprasanth <leelasaibheesetti@gmail.com>
2023-10-21 04:16:21 +0530

    retirved the json data from file to the application : httpClient Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { MatPaginatorModule} from '@angular/material/paginator';
 import {MatSortModule} from '@angular/material/sort';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatTableModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatSortModule],  
+  imports: [BrowserModule, MatTableModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatSortModule, HttpClientModule],  
   providers: [],
   bootstrap: [AppComponent],
 })
