11e6ba82c8745fa69d503758320248cd7d34c72f
leelasaiprasanth <leelasaibheesetti@gmail.com>
2023-10-21 16:09:05 +0530

    failing to load the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,55 @@
 // import { Component } from '@angular/core';
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { Birth, DataService } from './data.service';
+
 import { MatTableDataSource } from '@angular/material/table';
-import { HttpClient } from '@angular/common/http';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { HttpErrorResponse } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  title = 'curd-app';
+  // data: Array<any> = []
+  dataSource = new MatTableDataSource<Birth>();
+
+  displayedColumns: string[] = ['id', 'name', 'gender', 'date'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  @ViewChild(MatSort) sort: MatSort | undefined;
+
+  constructor(private dataService: DataService) {
+  //   this.dataService.getItems().subscribe((res: any)=>{
+  //     // alert(JSON.stringify(res))
+  //     // this.data = res;
+  //     console.log(res);
+  // });
+  }
+
+  ngOnInit() {
+    this.dataService.getItems().subscribe((data) => {
+      this.dataSource.data = data;
+      if (this.paginator) {
+        this.dataSource.paginator = this.paginator;
+      }
+      if (this.sort) {
+        this.dataSource.sort = this.sort;
+      }
+    });
+    (error: HttpErrorResponse) => {
+      console.error(error);  
+    }
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+
 import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatTableModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
