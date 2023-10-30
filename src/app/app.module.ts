97ef62a92e57f346ac785eeb6e69e29c01c67eb2
Leela Sai Prasanth <leelasaibheesetti@gmail.com>
2023-10-30 13:16:15 +0530

    Initial MatTable: working dataSource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatTableModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatSortModule],  
+  imports: [BrowserModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
